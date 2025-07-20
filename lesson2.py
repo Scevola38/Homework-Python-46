74896c6844e1f83d72441b5579acd71da34c8361
DmitriySokolov <Chelsey1986@yandex.ru>
2025-07-21 01:49:49 +0300

    HomeWork 2

--- lesson2.py
+++
@@ -1,8 +0,0 @@
-#получить от пользователя оценку
-rate_as_str = input("Оценить работу оператора от 1 до 5: ")
-rate = int(rate_as_str)
-if(rate<1):
-   rate = 1
-if(rate>5):
-   rate = 5
-print(rate)
