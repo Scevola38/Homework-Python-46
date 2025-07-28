9fdf330770a01943e393aa3e25b2b757a64723e4
DmitriySokolov <Chelsey1986@yandex.ru>
2025-07-28 15:17:49 +0300

    Changes

--- lesson_2_task_2.py
+++
@@ -1,6 +0,0 @@
-def is_year_leap(year):
-    return True if year % 4 == 0 else False
-
-yr = int(input("Введите год: "))
-result = is_year_leap(yr)
-print(f"год {yr}:{result}")
--- lesson_2_task_3.py
+++
@@ -1,6 +0,0 @@
-import math
-
-def squar(side):
-    return math.ceil(side * side)
-num_side = int(input("Введите значение стороны квадрата: "))
-print(f"Площадь квадрата: {squar(num_side)}")
